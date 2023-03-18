c8af75dc8b118780178cd0e0d370087a2988b05c
Carlos Domiciano <carlos.dominciano@bandtec.com.br>
2023-03-18 00:33:20 -0300

    Projeto iniciado

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/MainActivity.kt
@@ -0,0 +1,12 @@
+package school.sptech.renthouse
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
