3acaf4f39b9a7551cf62af87290d80e6de0bd222
Silva, Gustavo Lura da <gustavo.lura.d.silva@accenture.com>
2023-03-26 22:13:05 -0300

    tela 1 de negociacao completa

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/NegociationActivityStepOne.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NegociationActivityStepOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_negociation_step_one)
+    }
+}
