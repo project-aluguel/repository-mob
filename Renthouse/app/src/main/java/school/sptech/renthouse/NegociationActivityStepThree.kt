39e6e67b63fc9924fe5ffaa6524c0aeeb06c030e
Silva, Gustavo Lura da <gustavo.lura.d.silva@accenture.com>
2023-03-28 20:33:50 -0300

    finalizado os negociation

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/NegociationActivityStepThree.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NegociationActivityStepThree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_negociation_step_three)
+    }
+}
