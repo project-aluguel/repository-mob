a0a9fe3f49e723bc8d9f2775f6fba02f369ce45c
Silva, Gustavo Lura da <gustavo.lura.d.silva@accenture.com>
2023-04-24 20:26:36 -0300

    tela de modal

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/ModalHomeActivity.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModalHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modal_home)
+    }
+}
