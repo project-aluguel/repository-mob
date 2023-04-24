72f4ab1b9381210c9b89c75259c1957229f04f29
CindyKanashiro <cindykanashiro@gmail.com>
2023-04-23 23:20:13 -0300

    Juntando branch Cindy com a do Enzo

--- Renthouse/app/src/main/java/school/sptech/renthouse/activity_wallet.kt
+++ Renthouse/app/src/main/java/school/sptech/renthouse/activity_wallet.kt
@@ -9,5 +9,31 @@ class activity_wallet : AppCompatActivity() {
         setContentView(R.layout.activity_wallet)
         // Esconde a barra de ação
         supportActionBar?.hide()
+        buscaSaldo("9c72f13f-2d15-49f3-8e3a-7ccf5ab78a54")
+    }
+
+    fun buscaSaldo(id:String){
+
+        val valorCarteira = findViewById<TextView>(R.id.valorCarteira)
+
+        val call = Apis.apiCarteira().buscaCarteira(id)
+
+        call.enqueue(object : Callback<CarteiraUsuario>{
+            override fun onResponse(
+                call: Call<CarteiraUsuario>,
+                response: Response<CarteiraUsuario>
+            ) {
+                if (response.isSuccessful){
+                    val idCarteira = response.body()?.idCarteira.toString()
+                    response.body()?.let {
+                        valorCarteira.text = it.saldoCarteira.toString()
+                    }
+                }
+            }
+            override fun onFailure(call: Call<CarteiraUsuario>, t: Throwable) {
+                println(" -----------------------------------------" + t.message)
+                t.printStackTrace()
+            }
+        })
     }
 }
--- Renthouse/app/src/main/java/school/sptech/renthouse/activity_wallet2.kt
+++ Renthouse/app/src/main/java/school/sptech/renthouse/activity_wallet2.kt
@@ -2,6 +2,13 @@ package school.sptech.renthouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class activity_wallet2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +16,63 @@ class activity_wallet2 : AppCompatActivity() {
         setContentView(R.layout.activity_wallet2)
         // Esconde a barra de ação
         supportActionBar?.hide()
+        buscaSaldo("9c72f13f-2d15-49f3-8e3a-7ccf5ab78a54")
+    }
+
+    var idCarteira = ""
+
+    fun buscaSaldo(idUsuario:String){
+
+        val valorCarteira = findViewById<TextView>(R.id.valorCarteira)
+
+        val call = Apis.apiCarteira().buscaCarteira(idUsuario)
+
+        call.enqueue(object : Callback<CarteiraUsuario> {
+            override fun onResponse(
+                call: Call<CarteiraUsuario>,
+                response: Response<CarteiraUsuario>
+            ) {
+                if (response.isSuccessful){
+                    idCarteira = response.body()?.idCarteira.toString()
+                    response.body()?.let {
+                        valorCarteira.text = it.saldoCarteira.toString()
+                    }
+                }
+            }
+            override fun onFailure(call: Call<CarteiraUsuario>, t: Throwable) {
+                println(" -----------------------------------------" + t.message)
+                t.printStackTrace()
+            }
+        })
+    }
+
+    fun recargaCarteira(component: View){
+
+        val recarga = findViewById<EditText>(R.id.recarga)
+        val call = Apis.apiCarteira().recarregaCarteira(idCarteira, RecargaCarteira(recarga.toString().toDouble()))
+
+        call.enqueue(object : Callback<CarteiraUsuario>{
+
+            override fun onResponse(
+                call: Call<CarteiraUsuario>,
+                response: Response<CarteiraUsuario>
+            ) {
+                if (response.isSuccessful){
+                    response.body()?.let {
+                        finish();
+                        overridePendingTransition(0, 0);
+                        startActivity(getIntent());
+                        overridePendingTransition(0, 0);
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<CarteiraUsuario>, t: Throwable) {
+                println(" -----------------------------------------" + t.message)
+                t.printStackTrace()
+            }
+
+        })
+
     }
 }
