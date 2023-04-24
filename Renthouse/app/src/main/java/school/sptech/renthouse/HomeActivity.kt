e65abc887a02a6bc6b8959f49d91d905bdde83ea
carlos dominciano <carlos.dominciano@bandtec.com.br>
2023-04-24 18:02:25 -0300

    subida pq sim

--- Renthouse/app/src/main/java/school/sptech/renthouse/HomeActivity.kt
+++ Renthouse/app/src/main/java/school/sptech/renthouse/HomeActivity.kt
@@ -7,27 +7,25 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-
-        val myFragment = PosterFragment()
-        fragmentTransaction.add(R.id.fragment_container_home, myFragment)
-        fragmentTransaction.commit()
-
-
     }
 
     fun getAllItens() {
         Apis.getApiUsuarios()
         val apiItens = Apis.getApiItens()
         var listItens:List<Item> = listOf()
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val myFragment = PosterFragment()
+        fragmentTransaction.add(R.id.fragment_container_home, myFragment)
+        fragmentTransaction.commit()
     }
+
+
 
     override fun onResume() {
         super.onResume()
-
-
+        getAllItens()
     }
 
 
