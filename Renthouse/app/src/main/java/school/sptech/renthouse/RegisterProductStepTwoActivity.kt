a1794ae8e5a46621caa71c343b0429f90df17f1e
Silva, Gustavo Lura da <gustavo.lura.d.silva@accenture.com>
2023-05-04 23:16:15 -0300

    cadastrar produto estatico

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/RegisterProductStepTwoActivity.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterProductStepTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_product_step_two)
+    }
+}
