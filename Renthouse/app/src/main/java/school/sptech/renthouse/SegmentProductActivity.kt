4bdb6185665d9a4691255a4abab01687e267ef82
Silva, Gustavo Lura da <gustavo.lura.d.silva@accenture.com>
2023-05-06 19:43:13 -0300

    favoritos e tags

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/SegmentProductActivity.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegmentProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segment_product)
+    }
+}
