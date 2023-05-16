f757306629d92efad147422bc372fce35acb5272
carlos dominciano <carlos.dominciano@bandtec.com.br>
2023-05-16 14:50:48 -0300

    subindo para pegar coisa em outra bracnh

--- Renthouse/app/src/main/java/school/sptech/renthouse/HomeActivity.kt
+++ Renthouse/app/src/main/java/school/sptech/renthouse/HomeActivity.kt
@@ -3,8 +3,10 @@ package school.sptech.renthouse
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import retrofit2.Call
 import android.view.View
+import android.view.ViewGroup
 
 class HomeActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,25 @@ class HomeActivity() : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_home)
 
+
+        val i1:ItemRequest? = null
+        val i2:ItemRequest? = null
+        val i3:ItemRequest? = null
+        val i4:ItemRequest? = null
+        val itens = listOf(i1, i2, i3, i4);
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        for (item in itens) {
+            val myFragment = PosterFragment()
+            val argumentos = Bundle()
+            argumentos.putSerializable("item", item)
+            myFragment.arguments = argumentos
+            fragmentTransaction.add(R.id.fragment_container_home, myFragment)
+        }
+
+        fragmentTransaction.commit()
     }
 
 //    fun getAllItens() {
