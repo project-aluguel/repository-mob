102f6621cc2ba6b7617e21aa65a839d6a1f69134
enzoSilvestre <enzo.silvestre@accenture.com>
2023-05-20 12:49:18 -0300

    adicionando a pagina

---
+++ Renthouse/app/src/main/java/school/sptech/renthouse/my_request.kt
@@ -0,0 +1,11 @@
+package school.sptech.renthouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class my_request : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_request)
+    }
+}
